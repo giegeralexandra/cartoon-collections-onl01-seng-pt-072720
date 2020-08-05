726fd10c152a2fcae50e30eac354b87d2a8a4598
Alexandra Gieger <giegeralexandra@gmail.com>
2020-08-05 22:01:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def long_planeteer_calls(calls)
   calls.any?{|i| i.length >4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  strings.detect{|i| i.any?(cheese_types)}
 end
