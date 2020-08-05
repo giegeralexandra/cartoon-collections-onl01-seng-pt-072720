fa0f4d4080a9568cd013e8a3249a494bace2cd08
Alexandra Gieger <giegeralexandra@gmail.com>
2020-08-05 21:45:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,12 +3,16 @@ def roll_call_dwarves(dwarves)
   puts dwarves
 end
 
-def summon_captain_planet(veggies)
+def summon_captain_planet(calls)
   veggies.collect{|x| x.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  i = 0 
+  while
+    return calls[i] if yield(calls[i])
+    i+=1 
+  end
 end
 
 def find_the_cheese# code an argument here
