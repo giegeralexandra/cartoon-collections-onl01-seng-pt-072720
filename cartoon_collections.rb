815c133ec80813d20cc5f3bd6fcae8759f223a2e
Alexandra Gieger <giegeralexandra@gmail.com>
2020-08-05 21:59:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,10 +8,7 @@ def summon_captain_planet(calls)
 end
 
 def long_planeteer_calls(calls)
-  if calls.any?{|i| i.length >=4}
-    return true 
-  elsif calls.all?{|i| i.length <=4}
-    return false
+  calls.any?{|i| i.length >4}
 end
 
 def find_the_cheese# code an argument here
